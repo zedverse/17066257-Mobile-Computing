2a3671279de6788815e17dc9d596ef1f51e407f6
Zg18aam <zaidalikhan72@gmail.com>
2021-07-05 15:53:42 +0100

    Result Output implemented

--- app/src/main/java/com/example/mcasgapp/MainActivity.java
+++ app/src/main/java/com/example/mcasgapp/MainActivity.java
@@ -22,8 +22,8 @@ public class MainActivity extends AppCompatActivity {
     int number1 = r.nextInt(4);
     int number2 = r.nextInt(4);
     int result = 0;
-    TextView num1TextView, num2TextView, ansTextView;
-
+    TextView num1TextView, num2TextView, ansTextView, outputTextView;
+    MediaPlayer ring;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +32,11 @@ public class MainActivity extends AppCompatActivity {
         num1TextView = (TextView)findViewById(R.id.n1);
         num2TextView = (TextView)findViewById(R.id.n2);
         ansTextView = (TextView)findViewById(R.id.ans);
-        MediaPlayer ring= MediaPlayer.create(MainActivity.this,R.raw.music);
+        outputTextView= (TextView)findViewById(R.id.output);
+
+        ring = MediaPlayer.create(MainActivity.this,R.raw.music); // set music for app
         ring.start(); // for sound appeal
+        ring.setLooping(true);
         randomCheck(); // for generating random quiz
         hideAnimation(); // to hide the Winning view
         // for dragging apple objects
@@ -103,6 +106,7 @@ public class MainActivity extends AppCompatActivity {
 
     private View.OnTouchListener handleTouch = new View.OnTouchListener() {
         float dX, dY;
+
         @Override
         public boolean onTouch(View view, MotionEvent event) {
 
@@ -138,6 +142,7 @@ public class MainActivity extends AppCompatActivity {
 
      public void showAnimation(){// for starting animation
 
+
          View winView = (View) findViewById(R.id.winView);
          ImageButton playBtn = (ImageButton) findViewById(R.id.playButton);
          ImageView thumb = (ImageView) findViewById(R.id.thumb);
@@ -150,6 +155,7 @@ public class MainActivity extends AppCompatActivity {
          str1.setVisibility(View.VISIBLE);
          str2.setVisibility(View.VISIBLE);
          str3.setVisibility(View.VISIBLE);
+
 
          ScaleAnimation scale = new ScaleAnimation(0, 2, 0, 2);
          scale.setDuration(7200);
@@ -214,26 +220,36 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-        private final View.OnClickListener handleClick = new View.OnClickListener() {
+        private  View.OnClickListener handleClick = new View.OnClickListener() {
 
 
 
         @Override
         public void onClick(View v) {
+
             int temp = (int) v.getTag();
-            Toast.makeText(MainActivity.this, "Button clicked", Toast.LENGTH_SHORT).show();
+            outputTextView.clearAnimation();
+//            Toast.makeText(MainActivity.this, "Button clicked", Toast.LENGTH_SHORT).show();
 
 
             if (temp == result) {
 
                 Toast.makeText(MainActivity.this, "Well done, Correct answer!", Toast.LENGTH_SHORT).show();
                 ansTextView.setText(String.valueOf(result));
+                outputTextView.setText("Well done, Last answer was Correct!");
                 showAnimation();
 
 
+
             } else {
+
                 Toast.makeText(MainActivity.this, "Incorrect button", Toast.LENGTH_SHORT).show();
+                outputTextView.setText("Sorry, Incorrect answer!");
             }
+
+            AlphaAnimation fadeOut = new AlphaAnimation(1, 0);
+            fadeOut.setDuration(500);
+            outputTextView.startAnimation(fadeOut);
         }
     };
 
